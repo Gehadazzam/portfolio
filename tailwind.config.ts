e3fb3f0831cc3493952aaae0be5f39e3131b51e7
Gehadazzam <gehadgamalazzam@gmail.com>
2024-12-10 17:39:01 +0200

    chore: add colors palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,25 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'pinky': {
+          '50': '#fff1f4',
+          '100': '#fee5e9',
+          '200': '#fdceda',
+          '300': '#fba1b7',
+          '400': '#f87497',
+          '500': '#f04375',
+          '600': '#dd2161',
+          '700': '#bb1552',
+          '800': '#9c154a',
+          '900': '#861546',
+          '950': '#4b0622',
+        },
+        dark: '#2A3335',
+        light: '#F5F5F5',
+      }
+    },
   },
   plugins: [],
 }
