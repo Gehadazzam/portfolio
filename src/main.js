fe92c217ee26b80f41b471120b3242009992238f
Gehadazzam <gehadgamalazzam@gmail.com>
2024-12-11 19:13:40 +0200

    feature: routing is here!

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import i18n from '@/plugins/i18n.js'
+import  i18n  from '@/plugins/i18n.ts'
+import routes  from '@/routes/index.ts'
 
 
-createApp(App).use(i18n).mount('#app')
+createApp(App).use(i18n).use(routes).mount('#app')
--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,22 +1,21 @@
 import { createI18n } from 'vue-i18n';
 
 // Function to dynamically import and merge all files in a language folder
-function loadLocaleMessages() {
-  const locales = require.context('../locales', true, /[A-Za-z0-9-_,\s]+\.json$/i);
-  const messages = {};
+async function loadLocaleMessages(): Promise<Record<string, object>> {
+  const locales = import.meta.glob('../locales/[A-Za-z0-9-_,\\s]+\\.json');
+  const messages: Record<string, object> = {};
 
-  locales.keys().forEach((key) => {
-    // Extract the language code from the folder name (e.g., "en", "es")
+  for (const key in locales) {
     const matched = key.match(/\/([a-zA-Z-]+)\//);
     if (matched && matched[1]) {
       const locale = matched[1];
       if (!messages[locale]) {
         messages[locale] = {};
       }
-      // Merge each file into the corresponding locale
-      Object.assign(messages[locale], locales(key));
+      const module = await locales[key]();
+      Object.assign(messages[locale], module.default);
     }
-  });
+  }
 
   return messages;
 }
@@ -25,7 +24,7 @@ function loadLocaleMessages() {
 const i18n = createI18n({
   locale: 'en',         // Default language
   fallbackLocale: 'en', // Fallback language
-  messages: loadLocaleMessages(),
+  messages: await loadLocaleMessages(),
 });
 
 export default i18n;
