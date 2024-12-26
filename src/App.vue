bb1f818b3502a27d6699ac84b6529c1aebb3ebf1
Gehadazzam <gehadgamalazzam@gmail.com>
2024-12-26 02:22:31 +0200

    feat: init home page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
+<script setup lang="ts">
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
+</script>
+
 <template>
   <div id="app">
     <router-view></router-view>
   </div>
 </template>
-
-<script lang="ts">
-export default {
-  name: 'App',
-};
-</script>
--- src/views/MyHome.vue
+++ src/views/MyHome.vue
@@ -1,5 +1,12 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 <template>
-  <h1>Home</h1>
+  <main class="bg-pinky-400 h-screen px-4 md:px-12 lg:px-24 text-white">
+    <div class="flex flex-col sm:flex-row items-center">
+      <div>
+        <h1 class="text-8xl my-8">{{ $t("name") }}</h1>
+        <p class="text-3xl">{{ $t("description") }}</p>
+      </div>
+      <img class="w-11/12 md:w-3/4" src="@/assets/kitty.svg" alt="Me" />
+    </div>
+  </main>
 </template>
