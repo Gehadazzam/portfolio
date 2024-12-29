524254833a76a523b66f0b87c15f937b5efe4ec5
Gehadazzam <gehadgamalazzam@gmail.com>
2024-12-29 18:10:00 +0200

    feat: add home icons

--- src/views/MyHome.vue
+++ src/views/MyHome.vue
@@ -1,12 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import HomeIcons from "../components/HomeIcons.vue";
+</script>
 <template>
-  <main class="bg-pinky-400 h-screen px-4 md:px-12 lg:px-24 text-white">
-    <div class="flex flex-col sm:flex-row items-center">
-      <div>
-        <h1 class="text-8xl my-8">{{ $t("name") }}</h1>
+  <main class="bg-black h-screen px-4 flex md:px-12 lg:px-24 text-white">
+    <div
+      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 my-auto gap-12 place-items-center"
+    >
+      <div class="col-span-2">
+        <h1 class="text-8xl font-Revolution my-8">{{ $t("name") }}</h1>
         <p class="text-3xl">{{ $t("description") }}</p>
       </div>
-      <img class="w-11/12 md:w-3/4" src="@/assets/kitty.svg" alt="Me" />
+      <div
+        class="bg-pinky-500 rounded-full flex w-[70%] justify-center items-center col-span-3 aspect-square"
+      >
+        <img class="w-full" src="@/assets/kitty.svg" alt="Me" />
+      </div>
+      <HomeIcons />
     </div>
   </main>
 </template>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,45 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
 module.exports = {
   content: [
-    './index.html', // Include the root HTML file
-    './src/**/*.{vue,js,ts,jsx,tsx}', // Include all source files
+    "./index.html", // Include the root HTML file
+    "./src/**/*.{vue,js,ts,jsx,tsx}", // Include all source files
   ],
   theme: {
     extend: {
       colors: {
-        'pinky': {
-          '50': '#fff1f4',
-          '100': '#fee5e9',
-          '200': '#fdceda',
-          '300': '#fba1b7',
-          '400': '#f87497',
-          '500': '#f04375',
-          '600': '#dd2161',
-          '700': '#bb1552',
-          '800': '#9c154a',
-          '900': '#861546',
-          '950': '#4b0622',
+        pinky: {
+          "50": "#fff1f4",
+          "100": "#fee5e9",
+          "200": "#fdceda",
+          "300": "#fba1b7",
+          "400": "#f87497",
+          "500": "#f04375",
+          "600": "#dd2161",
+          "700": "#bb1552",
+          "800": "#9c154a",
+          "900": "#861546",
+          "950": "#4b0622",
         },
-        dark: '#2A3335',
-        light: '#F5F5F5',
+        dark: "#2A3335",
+        light: "#F5F5F5",
       },
     },
+    fontFamily: {
+      Revolution: ["CustomFont", "sans-serif"],
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    require("@tailwindcss/typography"),
+    plugin(function ({ addBase }) {
+      addBase({
+        "@font-face": {
+          fontFamily: "CustomFont",
+          src: 'url("/src/assets/fonts/custom-font.woff2") format("woff2"), url("/src/assets/fonts/custom-font.woff") format("woff")',
+          fontWeight: "400",
+          fontStyle: "normal",
+        },
+      });
+    }),
+  ],
+};
